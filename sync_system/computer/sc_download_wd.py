5f2770cdf1a73de13638de46a1153d44b74b3225
SeungHyun Chae <seunghyunchae@SeungHyuns-MacBook-Air.local>
2020-08-14 23:01:15 +0900

    sc_download all file done

--- sync_system/computer/sc_download_wd.py
+++ sync_system/computer/sc_download_wd.py
@@ -4,17 +4,18 @@ from watchdog.events import FileSystemEventHandler
 
 import os
 
+WATCH_DIR = '/Users/seunghyunchae/UGRP/sync_system/computer/upload'
 SOURCE = 'shchae7@141.223.181.14:/home/shchae7/UGRP/sync_system/server/user_voice'
-DEST = '/Users/seunghyunchae/UGRP/sync_system/computer/download/' 
+DEST = '/Users/seunghyunchae/UGRP/sync_system/computer/download'
 
 class Watcher:
     def __init__(self):
         self.observer = Observer()
 
     def run(self):
-        print('Watcher started running!')
+        print('CS upload dir Watcher for Download started running!')
         event_handler = Handler()
-        self.observer.schedule(event_handler, SOURCE, recursive=True)
+        self.observer.schedule(event_handler, WATCH_DIR, recursive=True)
         self.observer.start()
 
         try:
@@ -31,13 +32,27 @@ class Handler(FileSystemEventHandler):
     @staticmethod
     def on_any_event(event):
         if event.event_type == 'created':
-            print("New file %s uploaded to SOURCE from APP!" % event.src_path)
-            os.system('rsync -chavzP -e \"ssh -p 7777\" ' + SOURCE + '/* ' + DEST)
+            print("New file %s uploaded to SOURCE from APP wait for DOWNLOAD!!!" % event.src_path)
 
-            #Execute Inference Python file
-            #print("New file %s downloaded from Computer!")
-            #os.systemu('python3 synthesizer.py --)
+            txt_file_size = os.stat(event.src_path).st_size
+
+            if txt_file_size < 50:
+                print('txt file size < 50')
+                time.sleep(3)
+            elif txt_file_size < 100:
+                print('txt file size < 100')
+                time.sleep(6)
+            elif txt_file_size < 150:
+                print('txt file size < 150')
+                time.sleep(9)
+            else:
+                print('txt file size > 150')
+                time.sleep(12)
+
+            print('slept')
+            os.system('rsync -chavzP -e "ssh -p 7777" ' + SOURCE + '/* ' + DEST)
+
 
 if __name__ == '__main__':
     w = Watcher()
-    w.run()	
+    w.run()
