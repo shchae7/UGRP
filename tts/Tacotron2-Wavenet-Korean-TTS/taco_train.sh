7e747889820431e93e0890a2076200e6107ccb92
SeungHyun Chae <shchae7@postech.ac.kr>
2020-09-12 19:29:39 +0900

    sh files update

--- tts/Tacotron2-Wavenet-Korean-TTS/taco_train.sh
+++ tts/Tacotron2-Wavenet-Korean-TTS/taco_train.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-#SBATCH -J KTacoSyn
-#SBATCH -o ./out_files/KTacoSyn.%j.out
+#SBATCH -J KTacoTr
+#SBATCH -o ./out_files/KTacoTr.%j.out
 #SBATCH -p gpu-all
-#SBATCH -t 1:00:00
-#SBATCH --gres=gpu:2
+#SBATCH -t 11-00:00:00
+#SBATCH --gres=gpu:4
 
-echo "Start Synthesizing Wav File"
+echo "Start Training Korean Tacotron2"
 echo "SLURM_SUBMIT_DIR = $SLURM_SUBMIT_DIR"
 echo "CUDA_HOME = $CUDA_HOME"
 echo "CUDA_VISIBLE_DEVICES = $CUDA_VISIBLE_DEVICES"
@@ -19,7 +19,7 @@ srun -l /bin/date
 conda init bash
 conda activate tf1-gpu-py36
 
-python synthesizer.py --load_path logs_tacotron2/train_sample --num_speakers 1 --speaker_id 0 --text "안녕하세요"
+python tacotron2_train.py
 
 date
 
