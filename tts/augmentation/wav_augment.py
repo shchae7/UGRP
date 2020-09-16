d8ae969523a3676a10d5b2c531e81d96ac6be730
SeungHyun Chae <shchae7@postech.ac.kr>
2020-09-16 22:30:27 +0900

    data augmentation works

--- tts/augmentation/wav_augment.py
+++ tts/augmentation/wav_augment.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 
 class AudioAugmentation:
     def read_audio_file(self, file_path):
-        input_length = 16000
+        #input_length = 16000
         data = librosa.core.load(file_path)[0]
-        if len(data) > input_length:
-            data = data[:input_length]
-        else:
-            data = np.pad(data, (0, max(0, input_length - len(data))), "constant")
+        #if len(data) > input_length:
+        #    data = data[:input_length]
+        #else:
+        #    data = np.pad(data, (0, max(0, input_length - len(data))), "constant")
         return data
 
-    def write_audio_file(self, file, data, sample_rate=16000):
+    def write_audio_file(self, file, data, sample_rate=22000):
         librosa.output.write_wav(file, data, sample_rate)
 
 
@@ -26,20 +26,20 @@ class AudioAugmentation:
         return np.roll(data, 1600)
 
     def stretch(self, data, rate=1):
-        input_length = 16000
+        #input_length = 16000
         data = librosa.effects.time_stretch(data, rate)
-        if len(data) > input_length:
-            data = data[:input_length]
-        else:
-            data = np.pad(data, (0, max(0, input_length - len(data))), "constant")
+        #if len(data) > input_length:
+        #    data = data[:input_length]
+        #else:
+        #    data = np.pad(data, (0, max(0, input_length - len(data))), "constant")
         return data
 
-    #def change_pitch(self, data):
-    #    bins_per_octave = 12
-    #    pitch_pm = 2
-    #    pitch_change =  pitch_pm * 2*(np.random.uniform())
-    #    data_pitch = librosa.effects.pitch_shift(data.astype('float64'), 16000, n_steps=pitch_change, bins_per_octave=bins_per_octave)
-    #    return data_pitch
+    def change_pitch(self, data):
+        bins_per_octave = 12
+        pitch_pm = 2
+        pitch_change =  pitch_pm * 2*(np.random.uniform())
+        data_pitch = librosa.effects.pitch_shift(data.astype('float64'), 22000, n_steps=pitch_change, bins_per_octave=bins_per_octave)
+        return data_pitch
             
 
     def plot_time_series(self, data):
@@ -64,7 +64,12 @@ data_roll = augmenter.shift(data)
 # Stretching the sound
 data_stretch = augmenter.stretch(data, 0.8)
 
+# Changing the pith of the sound
+data_pitch_change = augmenter.change_pitch(data)
+
 # Write generated cat sounds
-augmenter.write_audio_file('generated_cat1.wav', data_noise)
-augmenter.write_audio_file('generated_cat2.wav', data_roll)
-augmenter.write_audio_file('generated_cat3.wav', data_stretch)
+augmenter.write_audio_file('original_noise.wav', data)
+augmenter.write_audio_file('noise_added.wav', data_noise)
+augmenter.write_audio_file('noise_shifted.wav', data_roll)
+augmenter.write_audio_file('noise_stretched.wav', data_stretch)
+augmenter.write_audio_file('noise_pitch_changed.wav', data_pitch_change)
