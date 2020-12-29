86cdb608c9600a60342f5d6bc94a70c078dcf189
SeungHyun Chae <shchae7@postech.ac.kr>
2020-12-29 22:35:05 +0900

    server feedback check works

--- feedback_system/server/feedback/clear.sh
+++
@@ -1,2 +0,0 @@
-find . -name "*copy*" -exec rm -f {} \;
-
--- feedback_system/server/feedback_wd.py
+++ feedback_system/server/feedback_wd.py
@@ -5,14 +5,14 @@ from watchdog.events import FileSystemEventHandler
 
 import os
 
-SOURCE = 'feedback/'
+SOURCE = os.getcwd() + '/feedback'
 
 class Watcher:
     def __init__(self):
         self.observer = Observer()
 
     def run(self):
-        print('feedback dir Watcher started running!')
+        print('server feedback dir watcher running')
         event_handler = Handler()
         self.observer.schedule(event_handler, SOURCE, recursive=True)
         self.observer.start()
@@ -31,9 +31,9 @@ class Handler(FileSystemEventHandler):
     @staticmethod
     def on_any_event(event):
         if event.event_type == 'created':
-            print("New feedback file %s uploaded to SOURCE from COMPUTER!!!" % event.src_path)
-            os.system('python3 score_count.py ' + event.src_path + ' ' + str(len(os.listdir('./score'))+1))
-            print(event.src_path)
+            print("New feedback file %s uploaded to feedback/server/feedback from COMPUTER!!!" % event.src_path)
+            #print(event.src_path[len(SOURCE) + 2 : len(SOURCE) + 10])
+            os.system('python3 check_feedback.py ' + event.src_path[len(SOURCE) + 2 : len(SOURCE) + 10] + ' ' + str(len(os.listdir('./sat_feedback'))+1))
 
 
 if __name__ == '__main__':
