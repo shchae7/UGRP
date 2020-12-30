7f58f973a06f9a7eadae0a94853d102130f8fdc6
SeungHyun Chae <shchae7@postech.ac.kr>
2020-12-30 20:23:52 +0900

    add_to_dataset works

--- feedback_system/server/add_to_dataset.py
+++ feedback_system/server/add_to_dataset.py
@@ -6,11 +6,40 @@ USERID = 'shchae7'
 SAT_FEEDBACK = '/home/' + USERID + '/UGRP/feedback_system/server/sat_feedback/'
 WAV_SOURCE = '/home/' + USERID + '/UGRP/sync_system/server/user_voice/'
 WAV_DEST = '/home/' + USERID + '/UGRP/tts/Tacotron2-Wavenet-Korean-TTS/datasets/son/audio/'
-
-for filename in os.listdir(SAT_FEEDBACK):
-    print(filename)
-    shutil.copy(WAV_SOURCE + filename[:-4] + '.wav', WAV_DEST)
-    os.remove(SAT_FEEDBACK + filename)
+REL_WAV_DEST = './datasets/son/audio/'
 
 # writing to dataset
-#TXT_SOURCE = '/home/' + USERID + '/UGRP/sync_system/server/user_text/'
+TXT_SOURCE = '/home/' + USERID + '/UGRP/sync_system/server/user_text/'
+TXT_DEST = '/home/' + USERID + '/UGRP/tts/Tacotron2-Wavenet-Korean-TTS/datasets/son/son-recognition-All.json'
+
+for filename in os.listdir(SAT_FEEDBACK):
+    #print(filename)
+    shutil.copy(WAV_SOURCE + filename[:-4] + '.wav', WAV_DEST)
+
+    user_txt_file = open(TXT_SOURCE + filename, "r")
+    user_text = user_txt_file.readlines()
+    #print(user_text)
+
+    ori_recog_file = open(TXT_DEST, "r")
+    ori_lines = ori_recog_file.readlines()
+    ori_recog_file.close()
+    #print(ori_lines)
+
+    del_line_recog_file = open(TXT_DEST, "w")
+    count = 0
+    for line in ori_lines:
+        if count == len(ori_lines) - 2:
+            del_line_recog_file.write(line.strip('\n') + ',\n')
+        else:
+            if line.strip('\n') != '}':
+                del_line_recog_file.write(line)
+        count = count + 1
+    del_line_recog_file.close()
+
+    recog_file = open(TXT_DEST, "a")
+    for line in user_text:
+        recog_file.write('    ' + '\"' + REL_WAV_DEST + filename[:-4] + '.wav\": ' + '\"' + line + '\"\n')
+    recog_file.write("}")
+    recog_file.close()
+
+    os.remove(SAT_FEEDBACK + filename)
