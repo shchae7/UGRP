de175b2fe6cfd81dc1208ab86398f07ddce4951e
SeungHyun Chae <shchae7@postech.ac.kr>
2021-01-07 21:51:27 +0900

    computer prep mod

--- computer_prep.sh
+++ computer_prep.sh
@@ -3,7 +3,7 @@ rm -rf ./training_end
 mkdir training_end
 
 # feedback_system
-mv ./feedback_system/computer/upload/20201229_110352.txt ./test_feedbacks
+mv ./feedback_system/computer/upload/20201229_110352.txt ./feedback_system/computer/test_feedbacks/
 
 #script
 mkdir ./script/computer/upload
@@ -12,4 +12,4 @@ mkdir ./script/computer/downloaded_script
 #sync_system
 rm -rf ./sync_system/computer/download
 mkdir ./sync_system/computer/download
-mv ./sync_system/computer/upload/20201229_110352.txt ./test_user_inputs
+mv ./sync_system/computer/upload/20201229_110352.txt ./sync_system/computer/test_user_inputs/
