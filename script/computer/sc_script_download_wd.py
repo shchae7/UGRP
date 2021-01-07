53860865ca72f427362708acb77e6d67f6cdcfcb
AllNorth-jeongwon <jeongwon@postech.ac.kr>
2021-01-07 17:22:20 +0900

    end alert ERROR

--- script/computer/sc_script_download_wd.py
+++ script/computer/sc_script_download_wd.py
@@ -36,6 +36,35 @@ class Watcher:
             self.observer.stop()
             print("observer stopped")
 
+    def run2(self):
+        print('Wait for creation of endfile')
+        event_handler = Handler()
+        self.observer.schedule(event_handler, SCRIPT_TXT_LINE, recursive=True)
+        self.observer.start()
+
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Observer Stopped!")
+
+            self.observer.join()
+    def run3(self):
+        while True:
+            os.system('rsync -chavzP -e \'ssh -p 7777\' ' + SCRIPT_TXT_LINE + '/end_of_training.txt ' + DEST)
+
+
+class Handler(FileSystemEventHandler):
+    @staticmethod
+    def on_any_event(event):
+        if event.event_type == 'created':
+            print("New file %s uploaded to SOURCE from APP!!!" % event.src_path)
+            # os.system('scp -r SOURCE DEST')
+            os.system('rsync -chavzP -e \'ssh -p 7777\' ' + SCRIPT_TXT_LINE + '/end_of_training.txt ' + DEST)
+
 if __name__ == '__main__':
     w = Watcher()
     w.run()
+    #w.run2()
+    w.run3()
