54cefa15af25e263dfc23be2c8dc13ca080b5c1c
AllNorth-jeongwon <jeongwon@postech.ac.kr>
2021-01-07 12:55:16 +0900

    making endfile in ../../script/server/script_line, 15s after starting trains

--- tts/Tacotron2-Wavenet-Korean-TTS/trigger_script_proceed.sh
+++ tts/Tacotron2-Wavenet-Korean-TTS/trigger_script_proceed.sh
@@ -2,17 +2,17 @@ srun -l /bin/hostname
 srun -l /bin/pwd
 srun -l /bin/date
 
-#make user directory
+#some directory settings
 rm -rf datasets/user/audio
+rm -rf ../../script/server/script_line/end_of_training.txt
 mkdir datasets/user/audio
 #preprocess
 conda init bash
 source activate tf1-demo-env
 
-python trigger_preprocess.py & sh datasets/user/mv_wav.sh
-
+#python trigger_preprocess.py & sh datasets/user/mv_wav.sh
+python trigger_preprocess.py
 date
-
 conda deactivate
 
 #wait for preprocessing
@@ -23,5 +23,9 @@ python trigger_taco2_train.py
 
 #start to train wavenet with user's data
 python trigger_wave_train.py
+
+#alert its end
+sleep 15 #15s
+echo 'Hello, world.' >../../script/server/script_line/end_of_training.txt
 
 echo "##### END #####"
